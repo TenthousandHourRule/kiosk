1f951ecdbdc46a3b169d9f31ba39253f2789f011
TenthousandHourRule <jung010209@gmail.com>
2025-06-24 16:43:06 +0900

    seconod commit

--- w1/src/cart.js
+++ w1/src/cart.js
@@ -1,25 +1,43 @@
 let cart = [];
 
 export function addCart(menu) {
-  console.log("add");
-  menu.qty = 1;
-  const targetMenu = cart.filter((obj) => obj.mno === menu.mno);
+  console.log("--------------------1");
+  console.log(menu);
 
-  if (targetMenu) {
-    targetMenu.qty++;
+  const targetMenuArr = cart.filter((obj) => obj.mno === menu.mno);
+  console.log("--------------------2");
+  console.log(targetMenuArr);
+
+  if (targetMenuArr.length !== 0) {
+    console.log("--------------------3-1");
+
+    targetMenuArr[0].qty = targetMenuArr[0].qty + 1;
   } else {
+    console.log("--------------------3-2");
     menu.qty = 1;
     cart = [...cart, menu];
   }
-  cart = [...cart, menu];
-}
-export function removeCart(mno) {
-  console.log("remove");
-  const targetMenu = cart.filter((obj) => obj.mno === menu.mno);
 
-  if (targetMenu) {
+  console.log(cart);
+
+  return cart;
+}
+export function changeCartQty(mno, oper) {
+  console.log("changeCartQty");
+
+  const targetMenu = cart.filter((obj) => obj.mno === Number(mno))[0];
+
+  console.log("------------eerserwer------------------");
+  console.log(targetMenu);
+
+  if (oper === "plus") {
+    targetMenu.qty += 1;
+  } else if (oper === "minus") {
     if (targetMenu.qty === 1) {
-      cart = filter((obj) => obj.mno !== mno);
+      cart = cart.filter((obj) => obj.mno !== Number(mno));
+    } else {
+      targetMenu.qty -= 1;
     }
   }
+  return cart;
 }
