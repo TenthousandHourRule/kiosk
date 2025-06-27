63d13177a31b26a8474f2b70d2f3072f63f3ed90
TenthousandHourRule <jung010209@gmail.com>
2025-06-27 16:29:20 +0900

    router setting

--- r3/src/main.jsx
+++ r3/src/main.jsx
@@ -1,12 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import router from './router/root'
-import { RouterProvider } from 'react-router'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import router from "./router/root";
+import { RouterProvider } from "react-router";
+import TestContextWrapper from "./store/testContext";
 
-
-
-
-
-createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />,
-)
+createRoot(document.getElementById("root")).render(
+  <TestContextWrapper>
+    <RouterProvider router={router} />,
+  </TestContextWrapper>
+);
